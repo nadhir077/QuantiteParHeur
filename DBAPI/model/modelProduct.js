df5f9488d9af07805f554ffdba86f9b824ddeef2
nadhir <nadhir.daoued@gmail.com>
2024-05-31 14:29:01 +0100

    second commit

--- DBAPI/model/modelProduct.js
+++ DBAPI/model/modelProduct.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
 const products = new mongoose.Schema({
     refProduit : String,
-    operation : String
+    operation : String,
+    temp:String
 });
 
 module.exports = mongoose.model('PRODUCTS',products)
